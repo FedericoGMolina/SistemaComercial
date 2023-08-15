23077f79b1691df42f64cca5f75cb8cfda3856b9
FedericoGMolina <federicofiszman@gmail.com>
2023-08-15 17:28:45 -0300

    Cambio de diseño (Tamaño de fuente, de formulario, etc) Además de agregar el mensaje de error al ingresar caracteres prohibidos. Tambien se añadió un logo de prueba para ver como queda en el lado izquierdo del formulario.

--- SistemaComercial/Interfaces/Login.Designer.cs
+++ SistemaComercial/Interfaces/Login.Designer.cs
@@ -34,13 +34,20 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.bunifuElipse2 = new Bunifu.Framework.UI.BunifuElipse(this.components);
             this.bunifuThinButton21 = new Bunifu.Framework.UI.BunifuThinButton2();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.panelUsuarioLogin = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.txtUsuarioLogin = new System.Windows.Forms.TextBox();
             this.txtContraseñaLogin = new System.Windows.Forms.TextBox();
-            this.panel3 = new System.Windows.Forms.Panel();
+            this.panelContraseñaLogin = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.lblErrorUsuarioLogin = new System.Windows.Forms.Label();
+            this.lblErrorContraseñaLogin = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // bunifuElipse1
@@ -51,11 +58,12 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(224)))), ((int)(((byte)(204)))));
+            this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(247, 366);
+            this.panel1.Size = new System.Drawing.Size(388, 361);
             this.panel1.TabIndex = 1;
             // 
             // bunifuElipse2
@@ -72,7 +80,7 @@
             this.bunifuThinButton21.ActiveLineColor = System.Drawing.Color.SeaGreen;
             this.bunifuThinButton21.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(50)))), ((int)(((byte)(68)))));
             this.bunifuThinButton21.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuThinButton21.BackgroundImage")));
-            this.bunifuThinButton21.ButtonText = "Sign Up";
+            this.bunifuThinButton21.ButtonText = "Login";
             this.bunifuThinButton21.Cursor = System.Windows.Forms.Cursors.Hand;
             this.bunifuThinButton21.Font = new System.Drawing.Font("Roboto", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bunifuThinButton21.ForeColor = System.Drawing.Color.White;
@@ -81,103 +89,170 @@
             this.bunifuThinButton21.IdleFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(217)))), ((int)(((byte)(197)))));
             this.bunifuThinButton21.IdleForecolor = System.Drawing.Color.Black;
             this.bunifuThinButton21.IdleLineColor = System.Drawing.Color.SeaGreen;
-            this.bunifuThinButton21.Location = new System.Drawing.Point(284, 263);
-            this.bunifuThinButton21.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bunifuThinButton21.Location = new System.Drawing.Point(457, 277);
+            this.bunifuThinButton21.Margin = new System.Windows.Forms.Padding(5);
             this.bunifuThinButton21.Name = "bunifuThinButton21";
-            this.bunifuThinButton21.Size = new System.Drawing.Size(91, 47);
+            this.bunifuThinButton21.Size = new System.Drawing.Size(109, 58);
             this.bunifuThinButton21.TabIndex = 4;
             this.bunifuThinButton21.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // panel2
+            // panelUsuarioLogin
             // 
-            this.panel2.BackColor = System.Drawing.Color.Gainsboro;
-            this.panel2.Location = new System.Drawing.Point(287, 125);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(262, 2);
-            this.panel2.TabIndex = 5;
+            this.panelUsuarioLogin.BackColor = System.Drawing.Color.Gainsboro;
+            this.panelUsuarioLogin.Location = new System.Drawing.Point(460, 149);
+            this.panelUsuarioLogin.Margin = new System.Windows.Forms.Padding(4);
+            this.panelUsuarioLogin.Name = "panelUsuarioLogin";
+            this.panelUsuarioLogin.Size = new System.Drawing.Size(349, 1);
+            this.panelUsuarioLogin.TabIndex = 5;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(297, 137);
+            this.label1.Location = new System.Drawing.Point(462, 160);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.Size = new System.Drawing.Size(0, 17);
             this.label1.TabIndex = 6;
             // 
             // txtUsuarioLogin
             // 
             this.txtUsuarioLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(50)))), ((int)(((byte)(68)))));
             this.txtUsuarioLogin.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtUsuarioLogin.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuarioLogin.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtUsuarioLogin.ForeColor = System.Drawing.Color.White;
-            this.txtUsuarioLogin.Location = new System.Drawing.Point(287, 106);
+            this.txtUsuarioLogin.Location = new System.Drawing.Point(460, 121);
+            this.txtUsuarioLogin.Margin = new System.Windows.Forms.Padding(4);
             this.txtUsuarioLogin.Name = "txtUsuarioLogin";
-            this.txtUsuarioLogin.Size = new System.Drawing.Size(262, 16);
+            this.txtUsuarioLogin.Size = new System.Drawing.Size(349, 25);
             this.txtUsuarioLogin.TabIndex = 7;
+            this.txtUsuarioLogin.TextChanged += new System.EventHandler(this.txtUsuarioLogin_TextChanged);
+            this.txtUsuarioLogin.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtUsuarioLogin_KeyPress);
             // 
             // txtContraseñaLogin
             // 
             this.txtContraseñaLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(50)))), ((int)(((byte)(68)))));
             this.txtContraseñaLogin.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtContraseñaLogin.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtContraseñaLogin.ForeColor = System.Drawing.Color.Gray;
-            this.txtContraseñaLogin.Location = new System.Drawing.Point(287, 169);
+            this.txtContraseñaLogin.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContraseñaLogin.ForeColor = System.Drawing.Color.White;
+            this.txtContraseñaLogin.Location = new System.Drawing.Point(464, 219);
+            this.txtContraseñaLogin.Margin = new System.Windows.Forms.Padding(4);
             this.txtContraseñaLogin.Name = "txtContraseñaLogin";
-            this.txtContraseñaLogin.Size = new System.Drawing.Size(262, 16);
+            this.txtContraseñaLogin.Size = new System.Drawing.Size(349, 25);
             this.txtContraseñaLogin.TabIndex = 9;
-            this.txtContraseñaLogin.Text = "Ingrese su contraseña";
+            this.txtContraseñaLogin.UseSystemPasswordChar = true;
+            this.txtContraseñaLogin.TextChanged += new System.EventHandler(this.txtContraseñaLogin_TextChanged);
+            this.txtContraseñaLogin.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtContraseñaLogin_KeyPress);
             // 
-            // panel3
+            // panelContraseñaLogin
             // 
-            this.panel3.BackColor = System.Drawing.Color.Gainsboro;
-            this.panel3.Location = new System.Drawing.Point(287, 188);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(262, 2);
-            this.panel3.TabIndex = 8;
+            this.panelContraseñaLogin.BackColor = System.Drawing.Color.Gainsboro;
+            this.panelContraseñaLogin.Location = new System.Drawing.Point(464, 245);
+            this.panelContraseñaLogin.Margin = new System.Windows.Forms.Padding(4);
+            this.panelContraseñaLogin.Name = "panelContraseñaLogin";
+            this.panelContraseñaLogin.Size = new System.Drawing.Size(349, 1);
+            this.panelContraseñaLogin.TabIndex = 8;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Roboto", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(283, 84);
+            this.label2.Location = new System.Drawing.Point(453, 87);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 19);
+            this.label2.Size = new System.Drawing.Size(96, 29);
             this.label2.TabIndex = 10;
             this.label2.Text = "Usuario";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Roboto", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(283, 147);
+            this.label3.Location = new System.Drawing.Point(456, 185);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(92, 19);
+            this.label3.Size = new System.Drawing.Size(137, 29);
             this.label3.TabIndex = 11;
             this.label3.Text = "Contraseña";
             // 
+            // lblErrorUsuarioLogin
+            // 
+            this.lblErrorUsuarioLogin.AutoSize = true;
+            this.lblErrorUsuarioLogin.Font = new System.Drawing.Font("Roboto", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblErrorUsuarioLogin.ForeColor = System.Drawing.Color.Red;
+            this.lblErrorUsuarioLogin.Location = new System.Drawing.Point(485, 156);
+            this.lblErrorUsuarioLogin.Name = "lblErrorUsuarioLogin";
+            this.lblErrorUsuarioLogin.Size = new System.Drawing.Size(0, 17);
+            this.lblErrorUsuarioLogin.TabIndex = 12;
+            // 
+            // lblErrorContraseñaLogin
+            // 
+            this.lblErrorContraseñaLogin.AutoSize = true;
+            this.lblErrorContraseñaLogin.Font = new System.Drawing.Font("Roboto", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblErrorContraseñaLogin.ForeColor = System.Drawing.Color.Red;
+            this.lblErrorContraseñaLogin.Location = new System.Drawing.Point(489, 255);
+            this.lblErrorContraseñaLogin.Name = "lblErrorContraseñaLogin";
+            this.lblErrorContraseñaLogin.Size = new System.Drawing.Size(0, 17);
+            this.lblErrorContraseñaLogin.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(466, 273);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 17);
+            this.label5.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Magneto", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(560, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(167, 53);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Login";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(65, 103);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(266, 148);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(50)))), ((int)(((byte)(68)))));
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(908, 361);
+            this.Controls.Add(this.lblErrorContraseñaLogin);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblErrorUsuarioLogin);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtContraseñaLogin);
-            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panelContraseñaLogin);
             this.Controls.Add(this.txtUsuarioLogin);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panelUsuarioLogin);
             this.Controls.Add(this.bunifuThinButton21);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = " ";
-            this.Load += new System.EventHandler(this.Login_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -188,13 +263,18 @@
         private System.Windows.Forms.Panel panel1;
         private Bunifu.Framework.UI.BunifuElipse bunifuElipse2;
         private Bunifu.Framework.UI.BunifuThinButton2 bunifuThinButton21;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panelUsuarioLogin;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtUsuarioLogin;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtContraseñaLogin;
-        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panelContraseñaLogin;
+        private System.Windows.Forms.Label lblErrorUsuarioLogin;
+        private System.Windows.Forms.Label lblErrorContraseñaLogin;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- SistemaComercial/Interfaces/Login.cs
+++ SistemaComercial/Interfaces/Login.cs
@@ -9,19 +9,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
+using System.Text.RegularExpressions;
 
 namespace SistemaComercial
 {
     public partial class Login : Form
     {
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        //Implementación de cue text
+        [DllImport("user32.dll", CharSet = CharSet.Auto)] 
         private static extern Int32 SendMessage(IntPtr hWnd, int msg, int wParam, [MarshalAs(UnmanagedType.LPWStr)] string lParam);
 
         public Login()
         {
             InitializeComponent();
-            SendMessage(txtUsuarioLogin.Handle, 0x1501, 0, "Username");     // EM_SETCUEBANNER
-            SendMessage(txtContraseñaLogin.Handle, 0x1501, 0, "Password");     // EM_SETCUEBANNER
+            SendMessage(txtUsuarioLogin.Handle, 0x1501, 0, "Username");            //Implementación de cue text
+            SendMessage(txtContraseñaLogin.Handle, 0x1501, 0, "Password");
             
         }
         public class Conexion
@@ -33,33 +35,70 @@ namespace SistemaComercial
             private string bd = "database=firstbdincsharp";
             public MySqlConnection getConexion()
             {
-
                 string cadenaConexion = servidor + ";" + puerto + ";" +
                 username + ";" + password + ";" + bd;
                 return new MySqlConnection(cadenaConexion);
             }
         }
-
         private void btnSalir_Click(object sender, EventArgs e)
         {
             this.Close();
         }
-
-        private void Login_Load(object sender, EventArgs e)
+        private void txtUsuarioLogin_KeyPress(object sender, KeyPressEventArgs e)
         {
+            Regex regex = new Regex(@"\W");
+            if (!char.IsControl(e.KeyChar) && regex.IsMatch(e.KeyChar.ToString()))
+            {
+                e.Handled = true;
+                panelUsuarioLogin.BackColor = Color.Red;
+                lblErrorUsuarioLogin.Text = "Solo se admiten caracteres Alfa Numericos";
+            }
+            else
+            {
+                lblErrorUsuarioLogin.Text = "";
+                panelUsuarioLogin.BackColor = Color.White;
+            }
 
+            if (e.KeyChar != '\b' && e.KeyChar == ' ' && regex.IsMatch(e.KeyChar.ToString()))
+                e.Handled = true;
         }
-
-        private void btnRegistrarse_Click(object sender, EventArgs e)
+        private void txtUsuarioLogin_TextChanged(object sender, EventArgs e)
         {
-            Register register = new Register();
-            register.Show();
-            this.Hide();
-            register.FormClosing += register_closing;
+            Regex regex = new Regex(@"\W");
+            if (regex.IsMatch(txtUsuarioLogin.Text))
+            {
+                txtUsuarioLogin.Text = "";
+                panelUsuarioLogin.BackColor = Color.Red;
+                lblErrorUsuarioLogin.Text = "Solo se admiten caracteres Alfa Numericos";
+            }
         }
-        private void register_closing(object sender, FormClosingEventArgs e)
+        private void txtContraseñaLogin_KeyPress(object sender, KeyPressEventArgs e)
         {
-            this.Show();
+            Regex regex = new Regex(@"\W");
+            if (!char.IsControl(e.KeyChar) && regex.IsMatch(e.KeyChar.ToString()))
+            {
+                e.Handled = true;
+                panelContraseñaLogin.BackColor = Color.Red;
+                lblErrorContraseñaLogin.Text = "Solo se admiten caracteres Alfa Numericos";
+            }
+            else
+            {
+                lblErrorContraseñaLogin.Text = "";
+                panelContraseñaLogin.BackColor = Color.White;
+            }
+
+            if (e.KeyChar != '\b' && e.KeyChar == ' ' && regex.IsMatch(e.KeyChar.ToString()))
+                e.Handled = true;
+        }
+        private void txtContraseñaLogin_TextChanged(object sender, EventArgs e)
+        {
+            Regex regex = new Regex(@"\W");
+            if (regex.IsMatch(txtContraseñaLogin.Text))
+            {
+                txtContraseñaLogin.Text = "";
+                panelContraseñaLogin.BackColor = Color.Red;
+                lblErrorContraseñaLogin.Text = "Solo se admiten caracteres Alfa Numericos";
+            }
         }
     }
 }
